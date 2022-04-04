7d6f07dffecc5cf6b6164129abe167bb374b002e
BT_1E10_30 <BT_1E10_30@bths.edu>
2022-04-04 13:41:01 -0400

    2nd commit

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,11 @@
 public class Runner {
     public static void main(String[] args)
     {
-        System.out.println(factorial(5));
-        System.out.println(factorial(10));
+        System.out.println(simpleRecursiveDivision(8));
+        System.out.println(sum(6));  // should print 21
+        System.out.println(sum(10)); // should print 55
+
+
     }
     public static int factorial(int num)
     {
@@ -13,4 +16,35 @@ public class Runner {
         }
         return returning;
     }
+    public static void simpleRecursion(int n)
+    {
+        System.out.println("Called, n = " + n);
+        if (n > 2)
+        {
+            simpleRecursion(n - 1);
+        }
+        System.out.println("Ended, n = " + n);
+    }
+    public static int simpleRecursiveDivision(int num)
+    {
+        if (num == 0)
+        {
+            return 0;
+        }
+        return num + simpleRecursiveDivision(num / 2);
+    }
+    public static int sum(int n)
+    {
+        if(n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return(n += sum(n-1));
+        }
+    }
+
+
+
 }
